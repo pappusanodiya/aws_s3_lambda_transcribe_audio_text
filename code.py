4bf6f6533c634633241c9a40f45b494e0423e921
Pappu sanodiya <81160267+pappusanodiya@users.noreply.github.com>
2023-06-28 04:22:30 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,32 @@
+import json
+import boto3
+import uuid
+
+
+transcribe = boto3.client('transcribe')
+
+def lambda_handler(event, context):
+    print("Hello guys I am Pappu Sanodiya....")
+    
+    filename = event['Records'][0]['s3']['object']['key']
+    bucketname = event['Records'][0]['s3']['bucket']['name']
+
+    url = "s3://" + bucketname + "/" + filename
+
+    myuuid = uuid.uuid1().int
+    print(event, url, bucketname)
+    response = transcribe.start_transcription_job(
+
+                    TranscriptionJobName="myaudiojob" + "-" + str(myuuid), 
+                    LanguageCode='en-US',
+                    MediaFormat='mp3',
+                    Media={
+                        'MediaFileUri': url,
+                            },
+                    OutputBucketName=bucketname,
+                    
+                    OutputKey="myaudiojob" + "-" + str(myuuid) + ".json"
+                    )
+    
+
+    print(response)
